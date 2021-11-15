abc9caa25a7332245a4cadc9364e92d8fa120d37
treatme030 <treatme8192@gmail.com>
2021-11-15 12:24:25 +0900

    feat : 메인 페이지 목업

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import PostList from '../components/main/PostList';
+import TagCategory from '../components/main/TagCategory';
+
+const MainpageBlock = styled.div`
+    width: 40%;
+    min-width: 320px;
+    margin: 0 auto;
+`;
 
 const MainPage = () => {
     return (
-        <div>
-            <h1>main page</h1>
-        </div>
+        <MainpageBlock>
+            <TagCategory/>
+            <PostList/>
+        </MainpageBlock>
     );
 };
 
