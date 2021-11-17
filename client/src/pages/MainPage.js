203dfb4a868cee295a051318b69a1dbb8fa316de
treatme030 <treatme8192@gmail.com>
2021-11-17 09:38:29 +0900

    edit : 기능 구현을 위한 코드 작성 후 주석처리

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Footer from '../components/common/Footer';
 import Nav from '../components/common/Nav';
@@ -12,12 +13,36 @@ const MainpageBlock = styled.div`
 `;
 
 const MainPage = () => {
+    // const dispatch = useDispatch();
+    
+    //useSelector로 posts 데이터와 user 데이터 받아오기
+    // const { posts, error, loading, user } = useSelector(({ posts, users }) => ({
+    //     posts: posts.posts,
+    //     error: posts.error,
+    //     loading: posts.loading,
+    //      user: users.user,
+    // }));
+
+    /*
+    location.search에 쿼리 확인 가능 
+    useEffect(() => {
+        const { hashtag } = qs.parse(location.search, {
+            ignoreQueryPrefix: true,
+        });
+        dispatch(getPosts());
+    },[dispatch, location.search]);
+    */
+
     return (
         <>
             <Nav/>
             <MainpageBlock>
                 <TagCategory/>
-                <PostList/>
+                <PostList
+                // loading={loading}
+                // error={error}
+                // posts={posts}
+                />
             </MainpageBlock>
             <Footer/>
         </>
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../components/common/Button';
 import PostComments from '../components/post/PostComments';
@@ -7,10 +8,10 @@ import Footer from '../components/common/Footer';
 import Nav from '../components/common/Nav';
 
 const PostPageBlock = styled.div`
-    width: 30%;
+    width: 40%;
     min-width: 320px;
     margin: 0 auto;
-    padding: 2rem;
+    padding: 2rem 4rem;
     margin-top: 2rem;
     background: #fff;
     box-shadow: 0 0 2px rgba(0, 0, 0, 0.1);
@@ -28,6 +29,36 @@ const PostPageBlock = styled.div`
 
 
 const PostPage = () => {
+    /*
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const { post, error, loading, comments, user } = useSelector(({ post, error, loading, comment }) => ({
+        post: post.post,
+        error: post.error,
+        loading: post.loading,
+        comments: comments.comments,
+        user: user.user
+    }));
+
+    useEffect(() => {
+        dispatch(viewPost(id));
+        return () => {
+            dispatch(initializeViewPost());
+        }
+    },[dispatch, id]);
+
+    if(!post){
+        return <PostPageBlock>존재하지 않는 글입니다.</PostPageBlock>;
+    }
+    if(error){
+        return <PostPageBlock>에러가 발생했습니다.</PostPageBlock>;
+    }
+    if(loading){
+        return <PostPageBlock>로딩중...</PostPageBlock>;
+    }
+
+    const { id, post_title, post_content, post_image, created_at, hashtags} = post
+    */
 
     return (
         <>
@@ -37,10 +68,19 @@ const PostPage = () => {
                     <h1>title</h1>
                     <span>2021.11.20</span>
                 </div>
-                <PostContents/>
-                <PostComments/>
+                <PostContents
+                //post={post}
+                />
+                <PostComments
+                //comments={comments}
+                //postId={id}
+                //user={user}
+                //changeInput={changeInput}
+                />
                 <div className="move-btn">
-                    <Button>메인 화면으로 이동</Button>
+                    <Link to="/main">
+                        <Button>메인 화면으로 이동</Button>
+                    </Link>
                 </div>
             </PostPageBlock>
             <Footer/>
