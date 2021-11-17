f3029a5cecba4efcdae2a61c33beeadc4184ccde
treatme030 <treatme8192@gmail.com>
2021-11-17 09:40:55 +0900

    feat : 서버 요청을 위한 api 작성

---
+++ client/src/lib/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+//글 등록 
+export const writePost = ({ title, post_image, post_content, hashtags }) =>
+    axios.post('http://localhost:4000/posts', { title, post_image, post_content, hashtags });
+
+//모든 글 불러오기
+export const getPosts = () => 
+    axios.get('http://localhost:4000/posts/list')
+
+/*
+export const getPosts = ( hashtag ) => 
+    axios.get(`http://localhost:4000/posts/list?hashtag=${hashtag}`)
+
+*/
+
+//특정 글 보기
+export const viewPost = id => axios.get(`http://localhost:4000/posts/${id}`);
+
+//특정 글 삭제하기
+export const removePost = id => axios.delete(`http://localhost:4000/posts/${id}`);
+
+//포스트에 댓글 추가하기
+export const writeComment = ({ user_id, id, comment_content }) =>
+    axios.post(`http://localhost:4000/posts/${id}/comments`, { user_id, comment_content });
+
+//쿨버튼 클릭
+export const clickCoolBtn = ({ user_id, id }) => 
+axios.post(`http://localhost:4000/posts/${id}/cool`, { user_id });
