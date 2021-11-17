0175079c8507422164910da197d62608fc6504a9
treatme030 <treatme8192@gmail.com>
2021-11-17 09:43:07 +0900

    edit : css 작성 위치 수정

--- client/src/components/common/CoolBtn.js
+++ client/src/components/common/CoolBtn.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FiThumbsUp } from 'react-icons/fi'
-export const CoolBtnContainer = styled.div`
-display:flex;
-justify-content: center;
-align-items: center;
-height: 20px;
-width: 50px;
-background-color: #FAFAFA;
-border: none;
-color: #00C6BC;
-font-weight: 700;
-border-radius: 40px;
-font-size: 14px;
-border: solid 1px #00C6BC;
-cursor: pointer;
-`
-export const BtnNum = styled.div`
-margin-top: 2px;
-display:flex;
-justify-content: center;
-align-items: flex-end;
-color: #00C6BC;
-margin-left: 5px;
-font-size: 10px;
-`
+
+const CoolBtnContainer = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height: 20px;
+    width: 50px;
+    background-color: #FAFAFA;
+    border: none;
+    color: #00C6BC;
+    font-weight: 700;
+    border-radius: 40px;
+    font-size: 14px;
+    border: solid 1px #00C6BC;
+    cursor: pointer;
+`;
+
+const BtnNum = styled.div`
+    margin-top: 2px;
+    display:flex;
+    justify-content: center;
+    align-items: flex-end;
+    color: #00C6BC;
+    margin-left: 5px;
+    font-size: 10px;
+`;
 
 const CoolBtn = () => {
     return (
