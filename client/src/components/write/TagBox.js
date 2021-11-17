6620530b2911e38b8e6e0d28e194f2bdb04011d3
treatme030 <treatme8192@gmail.com>
2021-11-17 16:23:40 +0900

    feat : 글쓰기 할 때 연관 태그 선택, 취소하기

--- client/src/components/write/TagBox.js
+++ client/src/components/write/TagBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from '../../style/palette';
 
 const TagBoxBlock = styled.div`
     margin-top: 2rem;
@@ -10,42 +11,38 @@ const TagBoxBlock = styled.div`
     }
     button {
         font-size: 1rem;
-        color: #b3b3b3;
-        border: 1px solid #e8e5e5;
+        color: ${palette.gray[4]};
+        border: 1px solid ${palette.gray[3]};
         border-radius: 20px;
         padding: 0.3rem 0.8rem;
         &:focus {
-            color: violet;
-            border: 1px solid violet;
+            color: ${palette.orange[2]};
+            border: 1px solid ${palette.orange[2]};
         }
     }
 `;
 
-const TagBox = () => {
+const categories = ['#일상', '#멋짐', '#사소', '#플렉스', '#뻔뻔'];
+const TagBox = ({ onclickTag, hashtags, onRemoveTag }) => {
+   
     return (
         <TagBoxBlock>
             <div>
                 <span>연관된 태그를 선택하세요.</span>
                 <ul className="tag-category">
-                    <li>
-                        <button type="button">#일상</button>
-                    </li>
-                    <li>
-                        <button type="button">#멋짐</button>
-                    </li>
-                    <li>
-                        <button type="button">#사소</button>
-                    </li>
-                    <li>
-                        <button type="button">#플렉스</button>
-                    </li>
-                    <li>
-                        <button type="button">#뻔뻔</button>
-                    </li>
+                    {categories.map((hashtag, idx) => (
+                        <li key={idx} onClick={() => onclickTag(hashtag)}>
+                            <button type="button" >{hashtag}</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
-            <ul>
-                {/* 위에서 선택한거 표시 */}
+            <ul className="tag-category">
+                {/* {hashtags.map((hashtag, idx) => (
+                    <li key={idx} onClick={() => onRemoveTag(idx)}>
+                        <button type="button">{hashtag}</button>
+                    </li>
+                ))} */}
             </ul>
         </TagBoxBlock>
     );
